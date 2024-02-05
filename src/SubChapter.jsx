65311e6d3650efa4df143c8ddbf32a9be9ac92bd
VitaliyLV <vitaliy.zubenyuk@gmail.com>
2024-02-05 19:32:18 +0200

    add info

--- src/SubChapter.jsx
+++ src/SubChapter.jsx
@@ -3,7 +3,9 @@ import "./SubChapter.css";
 export default function SubChapter({ info }) {
   return (
     <li>
-      <a href={`${info.Value}`}>{info.Name}</a>
+      <a href={`${info.Value}`} target="_blank" rel="noopener noreferrer">
+        {info.Name}
+      </a>
     </li>
   );
 }
